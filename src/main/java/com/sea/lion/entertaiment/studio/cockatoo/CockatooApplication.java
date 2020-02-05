8672d13fe27e584b82b906b606c26242d4d96d75
jose eduardo <joseleon1903@outlook.com>
2020-02-04 23:59:04 -0400

    init

---
+++ src/main/java/com/sea/lion/entertaiment/studio/cockatoo/CockatooApplication.java
@@ -0,0 +1,13 @@
+package com.sea.lion.entertaiment.studio.cockatoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CockatooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CockatooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sea/lion/entertaiment/studio/cockatoo/CockatooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sea.lion.entertaiment.studio.cockatoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CockatooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
